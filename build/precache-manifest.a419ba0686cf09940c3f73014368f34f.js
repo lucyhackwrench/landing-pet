fd871b466dc4097b221d5e6c57c3358b17e8916f
Liudmila Mzhachikh <l.mzhachikh@corp.mail.ru>
2020-01-15 15:35:07 +0300

    master add build dir to git (temporary for convenience)

---
+++ build/precache-manifest.a419ba0686cf09940c3f73014368f34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e3fbfb8cf6ff0e0e10be82e1127e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f71a3f4e018b66d94f1d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "da3db48a886bbdaa1ea3",
+    "url": "/static/js/2.d97c12cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d97c12cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f71a3f4e018b66d94f1d",
+    "url": "/static/js/main.ee53de2c.chunk.js"
+  },
+  {
+    "revision": "57b82acee458a8051a8b",
+    "url": "/static/js/runtime-main.5e3008aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
